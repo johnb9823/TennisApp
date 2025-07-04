cbe0e01de5fd4125e10faa869be3d485d0cd5bf1
Jaehoon <bjh0164@naver.com>
2025-07-04 16:44:57 +0900

    feat: 성공코드, 에러코드 추가

---
+++ src/main/java/com/example/tennisapp/global/success/SuccessCode.java
@@ -0,0 +1,44 @@
+package com.example.tennisapp.global.success;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum SuccessCode {
+	// 회원가입, 로그인
+	SIGNUP_SUCCESS(HttpStatus.OK, "회원가입 완료"),
+	LOGIN_SUCCESS(HttpStatus.OK, "로그인 성공"),
+	LOGOUT_SUCCESS(HttpStatus.OK, "로그아웃 성공"),
+	REFRESH_TOKEN(HttpStatus.OK, "엑세스 토큰 재발급 성공"),
+
+	// 회원
+	FIND_MEMBER_SUCCESS(HttpStatus.OK, "회원 조회 성공"),
+	UPDATE_MEMBER_SUCCESS(HttpStatus.OK, "회원정보 수정 성공"),
+	DELETE_MEMBER_SUCCESS(HttpStatus.OK, "회원 탈퇴 완료"),
+
+	// 리뷰
+	CREATE_REVIEW_SUCCESS(HttpStatus.OK, "리뷰 작성 성공"),
+	UPDATE_REVIEW_SUCCESS(HttpStatus.OK, "리뷰 수정 성공"),
+	GET_REVIEW_SUCCESS(HttpStatus.OK, "리뷰 상세 조회 성공"),
+	GET_ALL_REVIEWS_SUCCESS(HttpStatus.OK, "리뷰 전체 조회 성공"),
+	DELETE_REVIEW_SUCCESS(HttpStatus.OK, "리뷰 삭제 성공"),
+
+	// 신고
+	CREATE_REPORT_SUCCESS(HttpStatus.CREATED, "신고가 성공적으로 접수되었습니다."),
+
+	// 코트
+	COURT_CREATED(HttpStatus.CREATED, "코트 등록이 완료되었습니다."),
+	COURT_UPDATED(HttpStatus.OK, "코트 수정이 완료되었습니다."),
+	COURT_DELETED(HttpStatus.OK, "코트 삭제가 완료되었습니다."),
+	COURT_LIST_RETRIEVED(HttpStatus.OK, "근처 코트 목록 조회가 완료되었습니다."),
+
+	//정상작동
+	SUCCESS_OK(HttpStatus.OK, "요청이 정상적으로 처리되었습니다."),
+	DELETE_OK(HttpStatus.OK, "삭제가 정상적으로 처리되었습니다.");
+
+	private final HttpStatus httpStatus;
+	private final String message;
+}

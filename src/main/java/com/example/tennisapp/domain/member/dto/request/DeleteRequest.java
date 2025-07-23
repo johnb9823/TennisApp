4fa309c3fe6cc97a28c4112b07fad88af74d9c04
Jaehoon <bjh0164@naver.com>
2025-07-23 23:15:49 +0900

    fix: 회원 삭제시 soft delete 방식 오류 고침

--- src/main/java/com/example/tennisapp/domain/member/dto/request/DeleteRequest.java
+++ src/main/java/com/example/tennisapp/domain/member/dto/request/DeleteRequest.java
@@ -1,13 +1,17 @@
 package com.example.tennisapp.domain.member.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor // Jackson이 필요로 함
+@AllArgsConstructor
 public class DeleteRequest {
 
-	private final String password;
+	@NotBlank(message = "현재 비밀번호를 입력해주세요.")
+	private String password;
 
-	public DeleteRequest(String password) {
-		this.password = password;
-	}
+
 }

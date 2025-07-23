54695fcacbe222bd76225a43c7fa6516f8befb8c
Jaehoon <bjh0164@naver.com>
2025-07-24 00:50:59 +0900

    fix: Owner, member 테이블이 물지적으로 다른 테이블이라서 같은 이름, 같은 이메일로 가입이 되는 것을 수정했음

--- src/main/java/com/example/tennisapp/domain/member/dto/response/SignupResponse.java
+++ src/main/java/com/example/tennisapp/domain/member/dto/response/SignupResponse.java
@@ -1,17 +1,15 @@
 package com.example.tennisapp.domain.member.dto.response;
 
-import com.example.tennisapp.domain.member.entity.Member;
-
 import lombok.Getter;
 
 @Getter
-public class SignupResponseDto {
+public class SignupResponse {
 
 	private final Long id;
 	private final String name;
 	private final String email;
 
-	public SignupResponseDto(Long id, String name, String email) {
+	public SignupResponse(Long id, String name, String email) {
 		this.id = id;
 		this.name = name;
 		this.email = email;

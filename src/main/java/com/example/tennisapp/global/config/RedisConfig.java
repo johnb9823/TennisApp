82ea08d15b926145cd77569c0f8fe6d8310242ba
Jaehoon <bjh0164@naver.com>
2025-08-13 00:44:40 +0900

    feat: 동시성 문제 해결을 위한 redis 추가

---
+++ src/main/java/com/example/tennisapp/global/config/RedisConfig.java
@@ -0,0 +1,26 @@
+package com.example.tennisapp.global.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.redis.password}")
+    private String redisPassword;
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress("redis://localhost:6379")
+                .setPassword(redisPassword);
+        return Redisson.create(config);
+    }
+
+}
+
